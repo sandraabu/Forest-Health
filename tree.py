eae642fe1396e2da5acbb5d440bcdfe300b96893
Sandra Abubakir <83399849+sandraabu@users.noreply.github.com>
2022-06-21 21:35:08 +0100

    Add files via upload

--- tree.py
+++ tree.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-# import sklearn
+import sklearn
 # from sklearn.linear_model import LogisticRegressionCV
 # from sklearn.linear_model import LogisticRegression
  
